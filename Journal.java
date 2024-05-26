f8379f1c4255b0b7d10916cdc31e2a21eb9b7543
Charlene Manning <113045523+dyingshawarma@users.noreply.github.com>
2024-05-26 18:37:28 +0800

    Update Journal.java

--- Journal.java
+++ Journal.java
@@ -1,1 +1,8 @@
+public class Journal implements LibraryResource {
+    @Override
+    public void borrow(String title) {
+        // Implementation for borrowing a journal
+        System.out.println("Borrowing journal: " + title);
+    }
+}
 
